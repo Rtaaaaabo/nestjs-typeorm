709ca94900b5004674aeda212bce8ec8d48745c7
Taku Nakagawa <tk@www-biz.co>
2019-10-02 16:41:01 +0900

    Photo Serviceの作成

--- src/photo/photo.providers.ts
+++ src/photo/photo.providers.ts
@@ -1,1 +1,10 @@
 import { Connection, Repository } from 'typeorm';
+import { Photo } from './photo.entity';
+
+export const photoProviders = [
+  {
+    provide: 'PHOTO_PEPOSITORY',
+    useFactory: (connection: Connection) => connection.getRepository(Photo),
+    inject: ['DATABASE_CONNECTION'],
+  },
+];
--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Photo } from './photo.entity';
 
 @Injectable()
-export class PhotoService {}
+export class PhotoService {
+  constructor(
+    @Inject('PHOTO_REPOSITORY')
+    private readonly photoRepository: Repository<Photo>,
+  ) {}
+
+  async findAll(): Promise<Photo[]> {
+    return await this.photoRepository.find();
+  }
+}
