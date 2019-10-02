b2db32714ca004e4664e890070c252b2b3a60adf
Taku Nakagawa <tk@www-biz.co>
2019-10-02 16:07:52 +0900

    Photo entityの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { DatabaseProvider } from './database/database';
 import { DatabaseModule } from './database/database.module';
+import { PhotoService } from './photo/photo.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PhotoService],
   // providers: [...DatabaseProvider],
   // exports: [...DatabaseProvider],
 })
