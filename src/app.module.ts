b6eb4ab475430295ab30b4651f77fa1e20e7ce7d
Taku Nakagawa <tk@www-biz.co>
2019-10-02 15:59:56 +0900

    DataModule内を変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseProvider } from './database/database';
+// import { DatabaseProvider } from './database/database';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  // imports: [DatabaseModule],
-  // controllers: [AppController],
-  // providers: [AppService, Database],
-  providers: [...DatabaseProvider],
-  exports: [...DatabaseProvider],
+  imports: [DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+  // providers: [...DatabaseProvider],
+  // exports: [...DatabaseProvider],
 })
 export class AppModule {}
